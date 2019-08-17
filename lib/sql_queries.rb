c3954e77167c589e4c5edb5002afa94e39605f60
Jack Atherton <jackrrye@gmail.com>
2019-08-17 23:15:12 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,43 +1,43 @@
 #need help
 def selects_all_female_bears_return_name_and_age
-  "SELECT name, age FROM bears WHERE gender = 'Female'"
+  "SELECT name, age FROM bears WHERE gender = 'Female';"
 end
 
 
 
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "SELECT name FROM bears ORDER BY name"
+  "SELECT name FROM bears ORDER BY name;"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT name, age FROM bears WHERE alive = 1 ORDER BY age"
+  "SELECT name, age FROM bears WHERE alive = 1 ORDER BY age;"
 end
 
 
 
 #need help
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT name, age FROM bears WHERE age = (SELECT MAX(age) FROM bears)"
+  "SELECT name, age FROM bears WHERE age = (SELECT MAX(age) FROM bears);"
 end
 
 #why does this one work but max doesn't?
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT name, age FROM bears WHERE age = (SELECT MIN(age) FROM bears)"
+  "SELECT name, age FROM bears WHERE age = (SELECT MIN(age) FROM bears);"
 end
 
 
 
 #need help
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color FROM bears"
+  "SELECT color FROM bears;"
 end
 
 
 
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT COUNT(*) FROM bears WHERE temperament = 'goofy'"
+  "SELECT COUNT(*) FROM bears WHERE temperament = 'goofy';"
 end
 
 def selects_bear_that_killed_Tim
