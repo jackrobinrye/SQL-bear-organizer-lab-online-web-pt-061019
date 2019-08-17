0f3c2cd8214d5c38b5151ba3035081e2a3debc30
Jack Atherton <jackrrye@gmail.com>
2019-08-17 23:06:48 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -30,11 +30,11 @@ end
 
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  "SELECT color FROM BEARS"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "SELECT COUNT(*) FROM bears WHERE temperament = 'goofy'"
 end
 
 def selects_bear_that_killed_Tim
