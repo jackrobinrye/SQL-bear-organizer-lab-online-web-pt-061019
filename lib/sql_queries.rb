27321620a889eb2b160ea58896e80fab6620cd0b
Jack Atherton <jackrrye@gmail.com>
2019-08-17 23:26:04 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -30,7 +30,7 @@ end
 
 #need help
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color FROM bears;"
+  "SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY COUNT(*) DESC LIMIT 1;"
 end
 
 
