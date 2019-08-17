5edce9ce4cce012b67f3509a52a0e323d9aede00
Jack Atherton <jackrrye@gmail.com>
2019-08-17 23:31:41 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,10 +1,6 @@
-#need help
 def selects_all_female_bears_return_name_and_age
   "SELECT name, age FROM bears WHERE gender = 'F';"
 end
-
-
-
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
   "SELECT name FROM bears ORDER BY name;"
@@ -14,27 +10,17 @@ def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
   "SELECT name, age FROM bears WHERE alive = 1 ORDER BY age;"
 end
 
-
-
-#need help
 def selects_oldest_bear_and_returns_name_and_age
   "SELECT name, age FROM bears ORDER BY age DESC LIMIT 1;"
 end
 
-#why does this one work but max doesn't?
 def select_youngest_bear_and_returns_name_and_age
   "SELECT name, age FROM bears WHERE age = (SELECT MIN(age) FROM bears);"
 end
 
-
-
-#need help
 def selects_most_prominent_color_and_returns_with_count
   "SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY COUNT(*) DESC LIMIT 1;"
 end
-
-
-
 
 def counts_number_of_bears_with_goofy_temperaments
   "SELECT COUNT(*) FROM bears WHERE temperament = 'goofy';"
